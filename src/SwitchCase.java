1d7a10fb598e3b31f74a503686f80f0bc50727a1
TranVanTuan06 <24100557@st.phenikaa-uni.edu.vn>
2025-06-27 11:15:00 +0000

    fix

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -1,4 +1,4 @@
-package src;
+
 
 public class SwitchCase {
      public static void main(String[] args) {
--- src/breakandcontinue.java
+++ src/breakandcontinue.java
@@ -1,4 +1,4 @@
-package src;
+
 
 public class breakandcontinue {
      public static void main(String[] args) {
