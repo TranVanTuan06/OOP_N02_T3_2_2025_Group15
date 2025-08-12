27c2f130707598250203135285a23381859eb213
TranVanTuan06 <24100557@st.phenikaa-uni.edu.vn>
2025-08-12 16:40:57 +0000

    background

--- src/cafemanager/view/BackgroundPanel.java
+++ src/cafemanager/view/BackgroundPanel.java
@@ -1,5 +1,27 @@
 package cafemanager.view;
 
-public class BackgroundPanel {
-    
+import java.awt.*;
+import javax.swing.*;
+import java.net.URL;
+
+public class BackgroundPanel extends JPanel {
+    private Image backgroundImage;
+
+    public BackgroundPanel(String imagePath) {
+        // Lấy ảnh từ resource
+        URL imageUrl = getClass().getResource(imagePath);
+        if (imageUrl != null) {
+            backgroundImage = new ImageIcon(imageUrl).getImage();
+        } else {
+            System.out.println("Không tìm thấy ảnh: " + imagePath);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
 }
