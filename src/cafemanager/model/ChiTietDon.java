b9113e42d6be7b4f8bb0f7e43ec1c0b9d0b422bf
nguyensy1503 <24104291@st.phenikaa-uni.edu.vn>
2025-08-12 16:06:46 +0000

    bt2

--- src/cafemanager/model/ChiTietDon.java
+++ src/cafemanager/model/ChiTietDon.java
@@ -1,5 +1,28 @@
 package cafemanager.model;
 
 public class ChiTietDon {
-    
+
+    private Mon mon;
+    private int soLuong;
+
+    public ChiTietDon(Mon mon, int soLuong) {
+        this.mon = mon;
+        this.soLuong = soLuong;
+    }
+
+    public Mon getMon() {
+        return mon;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int solg) {
+         this.soLuong = solg;
+    }
+
+    public double tinhTien() {
+        return mon.getGia() * soLuong;
+    }
 }
